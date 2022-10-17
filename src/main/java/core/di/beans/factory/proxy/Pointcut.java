b990505338ab63987a7ef192be5c637898c80f8d
donghui.lee <donghui.lee@bugs.co.kr>
2022-10-18 00:08:27 +0900

    refactor: Advice, Pointcut 수정

--- src/main/java/core/di/beans/factory/proxy/Pointcut.java
+++ src/main/java/core/di/beans/factory/proxy/Pointcut.java
@@ -2,6 +2,6 @@ package core.di.beans.factory.proxy;
 
 import java.lang.reflect.Method;
 
-public interface PointCut {
-	boolean matches(Method m);
+public interface Pointcut {
+	boolean matches(Method method);
 }
